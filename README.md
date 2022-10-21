5679f1133f05684458d475e017540f22ff5cf2bb
BSR251196 <115452536+BSR251196@users.noreply.github.com>
2022-10-21 13:12:11 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# Axisbankproject-Maven
---
+++ src/main/java/SIDGSpkg/App.java
@@ -0,0 +1,14 @@
+package SIDGSpkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	System.out.println( "this message is from srikanth" );
+    }
+}
